6af2a1408a025140d0aabc33e3a309c6bf319144
Maxim Strekolovskiy <l0ka@live.ru>
2024-05-05 15:28:55 +0700

    improve

--- Employees-list/Program.cs
+++ Employees-list/Program.cs
@@ -1,89 +1,92 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employees_list
+namespace EmployeesList
 {
     internal class Program
     {
-        //Вывод сотрудников на экран
-        static void ReadStaff(string note)
+        // Вывод сотрудников на экран
+        static void ReadStaff()
         {
-            string[] employeesTxt = File.ReadAllLines("Сотрудники.txt");
+            string[] employeesTxt = File.ReadAllLines("Employees.txt");
+            Console.WriteLine("{0,-5} {1,-15} {2,-15} {3,-10} {4,-10} {5,-15} {6,-20}",
+                "ID", "Time Stamp", "Name", "Age", "Height", "Birth Date", "Birth Place");
             foreach (string employeeTxtString in employeesTxt)
             {
                 if (!String.IsNullOrWhiteSpace(employeeTxtString))
                 {
-                    Console.WriteLine($"{"ID"}{" Время записи"}{" Ф.И.О."}{" Возраст"}{" Рост"}{" Дата рождения"}{" Место рождения"}");
                     string[] data = employeeTxtString.Split(new string[] { "#" }, StringSplitOptions.None);
-                    Console.WriteLine($"{data[0]} {data[1]} {data[2]} {data[3]} {data[4]} {data[5]} {data[6]}\n");
+                    Console.WriteLine("{0,-5} {1,-15} {2,-15} {3,-10} {4,-10} {5,-15} {6,-20}",
+                        data[0], data[1], data[2], data[3], data[4], data[5], data[6]);
                 }
             }
         }
 
-        //Запись сотрудников
-        static void AddStaff(string note)
+        // Запись сотрудников
+        static void AddStaff()
         {
-            using (StreamWriter sw = new StreamWriter("Сотрудники.txt", true, Encoding.Unicode))
+            using (StreamWriter sw = new StreamWriter("Employees.txt", true, Encoding.Unicode))
             {
-                int ID = new int();
-                char AddKey = 'д';
+                int id = 1;
+                string note = String.Empty;
+                char addKey = 'д';
                 do
                 {
-                    ID++;
-                    Console.WriteLine($"\nID записи {ID}");
-                    note += $"\n{ID}#";
+                    Console.WriteLine("\nID записи: {0}", id);
+                    note += String.Format("{0}#", id);
 
                     string date = DateTime.Now.ToShortDateString();
                     string time = DateTime.Now.ToShortTimeString();
 
-                    Console.WriteLine($"Время записи {date} {time}");
-                    note += $"{date} {time}#";
+                    Console.WriteLine("Время записи: {0} {1}", date, time);
+                    note += String.Format("{0} {1}#", date, time);
 
                     Console.Write("Введите Ф.И.О. сотрудника: ");
-                    note += $"{Console.ReadLine()}#";
+                    note += String.Format("{0}#", Console.ReadLine());
 
                     Console.Write("Введите возраст сотрудника: ");
-                    note += $"{Console.ReadLine()}#";
+                    note += String.Format("{0}#", Console.ReadLine());
 
                     Console.Write("Введите рост сотрудника: ");
-                    note += $"{Console.ReadLine()}#";
+                    note += String.Format("{0}#", Console.ReadLine());
 
                     Console.Write("Введите дату рождения сотрудника: ");
-                    note += $"{Console.ReadLine()}#";
+                    note += String.Format("{0}#", Console.ReadLine());
 
                     Console.Write("Введите место рождения сотрудника: ");
-                    note += $"{Console.ReadLine()}";
-                    sw.Write(note);
+                    note += Console.ReadLine();
 
-                    Console.Write("\nПродолжить н/д\n");
-                    AddKey = Console.ReadKey(true).KeyChar;
+                    sw.WriteLine(note);
 
-                } while (char.ToLower(AddKey) == 'д');
+                    Console.Write("\nПродолжить (д/н)? ");
+                    addKey = Console.ReadKey(true).KeyChar;
+                    id++;
+                    note = String.Empty;
+
+                } while (char.ToLower(addKey) == 'д');
             }
         }
 
         static void Main(string[] args)
         {
-            string note = String.Empty;
-
             do
             {
                 Console.Write("Выберите пункт меню\n 1 - Список сотрудников\n 2 - Новая запись\n");
-                String OptKey = Console.ReadLine();
-                switch (OptKey)
+                String optKey = Console.ReadLine();
+                switch (optKey)
                 {
                     case "1":
                         Console.Write("\nСписок сотрудников\n");
-                        ReadStaff(note);
+                        ReadStaff();
                         break;
 
                     case "2":
                         Console.Write("\nНовая запись");
-                        AddStaff(note);
+                        AddStaff();
                         break;
                     default: Console.WriteLine("\nНеверный ввод"); continue;
                 }
